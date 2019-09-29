ac547e68bbf2d03cf34ed0b8e35a0375ef6f7649
Shubham <shubhamsinha2050@gmail.com>
2019-09-29 20:20:22 +0530

    Added list, details and form components for main movie module

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
 
 import { Routes, RouterModule } from '@angular/router';
+import { MovieDetailsComponent } from '../movie-details/movie-details.component';
+import { MovieFormComponent } from '../movie-form/movie-form.component';
+import { MovieListComponent } from '../movie-list/movie-list.component';
 
 const routes: Routes = [
   { path: 'main', component: MainComponent },
 ];
 
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [MainComponent, MovieDetailsComponent, MovieFormComponent, MovieListComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
