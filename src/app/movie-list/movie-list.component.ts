7fb8fa1535bb763bea295af2172fbb41468f05e7
Shubham <shubhamsinha2050@gmail.com>
2019-09-30 00:49:05 +0530

    added data in movie list nd going for a call from django api

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieListComponent implements OnInit {
 
+  movies = ['Terminator', 'Shermanator'];
+
   constructor() { }
 
   ngOnInit() {
